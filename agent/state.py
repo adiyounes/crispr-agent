419a77b1d06804f70095aa619f2058d7f62b3fc2
adiyounes <younesadi18@gmail.com>
2026-05-21 16:04:35 +0200

    state agent built (Important for LangGraph)

---
+++ agent/state.py
@@ -0,0 +1,28 @@
+from typing import TypedDict, Optional
+from dataclasses import dataclass, field
+
+@dataclass
+class OffTargetSite:
+    chromosome: str
+    position: int
+    sequence: str
+    mismatch_count: int
+    in_exon: bool
+    near_cancer_gene: bool
+    cancer_gene_bame: Optional[str]
+    site_risk_score: float
+
+class AgentState(TypeDict):
+    gene: str
+    variant:str
+    variant_id: Optional[str]
+
+    grna_sequence: Optional[str]
+    grna_reasoning: Optional[str]
+
+    off_targets: list[OffTargetSite]
+
+    risk_score: Optional[float]
+
+    verdict: Optional[float]
+    reasoning: Optional[str]
