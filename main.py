2280d60a9b32b469b81f8fa868e57dd69aeba874
adiyounes <younesadi18@gmail.com>
2026-06-03 14:07:31 +0200

    allow all origins in corsmiddleware

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.include_router(router, prefix="/api/v1/crispr")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
